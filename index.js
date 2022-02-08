a73b03dfd37f7771ce7a49abaa827fe9c2bec6ef
gracethedragon <gracethedragon@hotmail.com>
2022-02-07 19:46:46 -0800

    grace

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ let name = process.argv[2];
 console.log(`good day ${name}, are you well?`);
 I like to eat pie
 writeFile('data.json', name, (err) => console.error('err ->', err));
-console.log(`good day ${name}, are you well?`);
+console.log(`good day ${name}, are you well? yes i'm well`);
 
